429bf9302e2e3f983e5f1d0bbc7a680e45946e4c
redbirdfan <redbirdfan38@gmail.com>
2025-02-06 20:38:53 -0600

    Express connection to MySQL working. .ENV is not pulling user and PW correctly

--- src/Backend/server.js
+++ src/Backend/server.js
@@ -1,8 +1,41 @@
-import express from "express"
+require('dotenv').config();
+
+const express = require("express");
+const mysql = require("mysql2");
+const cors = require("cors");
 
 const app = express();
 
-app.get("/", (req, res) {
+const corsOptions = {
+    origin: "http://localhost:5000",
+    credentials: true
+}
+
+app.use(cors(corsOptions));
+app.use(express.json());
+
+const db =mysql.createConnection({ 
+    host: "localhost",
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
+});
+
+db.connect((err) => {
+        if(err) {
+                console.log("MySQL connection not made: ", err);
+            return;
+        }
+        console.log("Connect to MySQL");
+})
+
+const port = 5000;
+app.listen(port, () => {
+    console.log("Listening on port 5000")
+});
+
+
+app.get("/", (req, res) => {
     console.log("Server route functioning")
 })
 
